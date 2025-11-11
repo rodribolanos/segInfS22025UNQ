7f2f2ed222d8c95ef50c35307ce84814516c095e
nasrcristian <nasrcristian.un@gmail.com>
2025-11-11 13:49:39 -0300

    planteo de funciones de utilidad para la encriptacion y tabla de busqueda de sustitucion, ademas se escribio la signatura de las funciones principales

---
+++ tp-2/aes_utils.h
@@ -0,0 +1,32 @@
+
+#ifndef
+#define AES_H
+
+#define BLOCK_LEN 16 // Tamaño total del bloque de bytes
+#define BLOCK_SIDE 4 // Dimension del bloque de bytes
+
+
+// Matriz de sustitución que funciona como map con el inverso multiplicativo de cada valor dentro del cuerpo de 8 bits 
+extern unsigned char s_box[256];
+// Matriz de multiplicación al momento de mezclar las columnas del bloque a cifrar
+extern unsigned char mix_col_matrix[BLOCK_SIDE][BLOCK_SIDE];
+
+
+
+// Multiplicación cerrada entre dos bytes en el cuerpo  
+unsigned char closedMult(unsigned char b1, unsigned char b2);
+
+
+
+
+// ====== OPERACIONES DEFINIDAS PARA EL CIFRADO ======
+
+void addRoundKey(unsigned char state[BLOCK_SIDE][BLOCK_SIDE], unsigned char expanded_key[BLOCK_SIDE][BLOCK_SIDE]);
+void subByte(unsigned char state[BLOCK_SIDE][BLOCK_SIDE]);
+void mixColumns(unsigned char state[BLOCK_SIDE][BLOCK_SIDE])
+
+// ====== OPERACIONES DEFINIDAS PARA EL CIFRADO ======
+
+
+
+#endif
