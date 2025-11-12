3a3715726a22f25c57c39d0030caef6c2588bcf2
nasrcristian <nasrcristian.un@gmail.com>
2025-11-12 17:48:48 -0300

    movid e lugar la funcion

--- tp-2/main.c
+++ tp-2/main.c
@@ -2,32 +2,11 @@
 #include "array_rotation.h"
 
 
-void shiftRows(unsigned char state[BLOCK_SIDE][BLOCK_SIDE]){
-    leftRotate(state[1], 1, BLOCK_SIDE);
-    leftRotate(state[2], 2, BLOCK_SIDE);
-    rightRotate(state[3], 1, BLOCK_SIDE);
-} // tambien se puede hacer como
-
-void shiftRows2(unsigned char state[BLOCK_SIDE][BLOCK_SIDE]){
-    for(int i = 1; i < BLOCK_SIDE; i++){
-        leftRotate(state[i], i, BLOCK_SIDE); 
-        // de esta forma se rota 1, 2 y 3 veces a la izquierda respectivamente como esta especificado en el algoritmo
-    }
-}
-
-
-/*
-    state = [[]
-                []
-                    []
-                        []]
-*/
-
 int main(int argc, char* argv[]){
 
-    key[BLOCK_SIDE][BLOCK_SIDE];
+    key[BLOCK_DIM][BLOCK_DIM];
     // el main va a quedar algo como:
-    state[BLOCK_SIDE][BLOCK_SIDE]; // TODO: ejemplo
+    state[BLOCK_DIM][BLOCK_DIM]; // TODO: ejemplo
     addRoundKey(state, key);
 
     for(int round = 0; round < N; n++){
